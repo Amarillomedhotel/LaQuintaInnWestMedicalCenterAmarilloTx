2013e5e0b6e5bac1f99e63212f72ddbff39dd7d3
Qurban Haciyev <haciyevqurban97@gmail.com>
2024-03-30 02:13:08 +0400

    project complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+
 document.getElementById("determination_no").addEventListener("click", function() {
     document.getElementById("inconvenient_block").removeAttribute("hidden");
     document.getElementById("determination_check_block").setAttribute("hidden", "hidden");
@@ -7,3 +8,14 @@ document.getElementById("determination_yes").addEventListener("click", function(
     document.getElementById("review_sources_block").removeAttribute("hidden");
     document.getElementById("determination_check_block").setAttribute("hidden", "hidden");
 });
+
+
+function SendMail() {
+   
+    var params= {
+        message: document.getElementById('review_text').value,
+    }
+    emailjs.send("Message","Thanks you!",params).then(function(res) {
+   console.log("ok");
+    })
+}
