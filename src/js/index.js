2bb0845e85886bf0d18e16f2bc129a5a682a3b2f
Qurban Haciyev <haciyevqurban97@gmail.com>
2024-03-30 07:33:31 +0400

    final project

--- src/js/index.js
+++ src/js/index.js
@@ -32,3 +32,24 @@ function SendMail() {
 
 
 
+
+// Sayfa yüklendiğinde bu fonksiyon çağrılır
+window.onload = function() {
+    // Sayaç değerini depolamak için bir değişken oluşturalım
+    var count = localStorage.getItem('reviewCount');
+
+    // Eğer count değeri yoksa veya değeri sayısal bir değilse, varsayılan değeri 2000 olarak atayalım
+    if (!count || isNaN(count)) {
+        count = 2000;
+    }
+
+    // Sayaç değerini gösteren span elementini bulalım
+    var reviewCountSpan = document.getElementById("review_count");
+
+    // Sayaç değerini span elementinin içeriğine yazalım
+    reviewCountSpan.textContent = count + " reviews";
+
+    // Her sayfa yüklendiğinde sayaç değerini artıralım ve local storage'a kaydedelim
+    count++;
+    localStorage.setItem('reviewCount', count);
+};
