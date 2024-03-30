25e087814c27d0e636ca9b9dc9c67c11ff890825
Qurban Haciyev <haciyevqurban97@gmail.com>
2024-03-30 06:26:47 +0400

    updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,25 @@
-
 document.getElementById("determination_no").addEventListener("click", function() {
-    document.getElementById("inconvenient_block").removeAttribute("hidden");
-    document.getElementById("determination_check_block").setAttribute("hidden", "hidden");
+    document.getElementById("inconvenient_block").style.display = "block";
+    document.getElementById("determination_check_block").style.display = "none";
 });
 
 document.getElementById("determination_yes").addEventListener("click", function() {
-    document.getElementById("review_sources_block").removeAttribute("hidden");
-    document.getElementById("determination_check_block").setAttribute("hidden", "hidden");
+    document.getElementById("review_sources_block").style.display = "block";
+    document.getElementById("determination_check_block").style.display = "none";
 });
 
-
 function SendMail() {
-   
-    var params= {
+    var inconvenientBoxDiv = document.getElementById("inconvenient_box");
+    var thanksDiv = document.getElementById("thanksDiv");
+
+    inconvenientBoxDiv.style.display = "none";
+    thanksDiv.style.display = "block";
+
+    var params = {
         message: document.getElementById('review_text').value,
     }
-    emailjs.send("Message","Thanks you!",params).then(function(res) {
-   console.log("ok");
+
+    emailjs.send("Message", "Thanks you!", params).then(function(res) {
+        console.log("ok");
     })
 }
